ea4b697491ea5379c082c29ad9e1685363de9896
aldiyaramanzhol <aldi12062006@gmail.com>
2025-05-18 21:07:21 +0500

    WeightedGraph.java

---
+++ src/WeightedGraph.java
@@ -0,0 +1,28 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeightedGraph<V> {
+    private Map<V, Vertex<V>> vertices = new HashMap<>();
+
+    public void addVertex(V data) {
+        vertices.putIfAbsent(data, new Vertex<>(data));
+    }
+
+    public void addEdge(V source, V dest, double weight) {
+        addVertex(source);
+        addVertex(dest);
+        Vertex<V> sourceVertex = vertices.get(source);
+        Vertex<V> destVertex = vertices.get(dest);
+        sourceVertex.addAdjacentVertex(destVertex, weight);
+    }
+
+    public Vertex<V> getVertex(V data) {
+        return vertices.get(data);
+    }
+
+    public Collection<Vertex<V>> getAllVertices() {
+        return vertices.values();
+    }
+}
+
